ad776e2cd34153fd24dda0c0bb5b9ba4dd5966c7
Angela Shao <annchies@usc.edu>
2016-10-24 21:32:23 -0700

    + CarType, added provider fragments

--- app/src/main/java/csci310/parkhere/resource/CarType.java
+++ app/src/main/java/csci310/parkhere/resource/CarType.java
@@ -5,5 +5,21 @@ package csci310.parkhere.resource;
  */
 
 public enum CarType {
+    Handicap(0x0001),
+    Compact(0x0002),
+    SUV(0x0004),
+    Truck(0x0008),
+    CoveredParking(0x0010),
+    AnyCarType(0xFFFF);
+
+    private int value;
+
+    CarType(int v) {
+        value = v;
+    }
+
+    public int getValue() {
+        return value;
+    }
 
 }
--- app/src/main/java/csci310/parkhere/ui/ProviderActivity.java
+++ app/src/main/java/csci310/parkhere/ui/ProviderActivity.java
@@ -10,6 +10,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import csci310.parkhere.R;
@@ -17,11 +18,14 @@ import csci310.parkhere.R;
 /**
  * Created by ivylinlaw on 10/17/16.
  */
-public class ProviderActivity extends AppCompatActivity implements SpacesFragment.OnFragmentInteractionListener {
+public class ProviderActivity extends AppCompatActivity implements SpacesFragment.OnFragmentInteractionListener, PrivateProfileFragment.OnFragmentInteractionListener {
+    ImageView _profilePic;
     TextView _spaceLink;
+
     FragmentManager fm;
     FragmentTransaction fragmentTransaction;
     Fragment spacesFragment;
+    Fragment privateProfileFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,19 +36,38 @@ public class ProviderActivity extends AppCompatActivity implements SpacesFragmen
         setSupportActionBar(renterToolbar);
 //
         _spaceLink = (TextView)findViewById(R.id.spaceLink);
+        _profilePic = (ImageView)findViewById(R.id.profilePic);
 //
         fm = getSupportFragmentManager();
         fragmentTransaction = fm.beginTransaction();
 //        spacesFragment = fm.findFragmentById(R.id.fragment_spaces);
         spacesFragment = new SpacesFragment();
+        privateProfileFragment = new PrivateProfileFragment();
 
-        fragmentTransaction.add(R.id.spaceFragContainer, spacesFragment);
-        fragmentTransaction.commit();
+//        fragmentTransaction.add(R.id.fragContainer, spacesFragment);
+//        fragmentTransaction.commit();
 
         _spaceLink.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                System.out.println("Clicked on space tab item");
+//                fragmentTransaction.replace(R.id.fragContainer, spacesFragment);
+//                fragmentTransaction.commit();
+            }
+        });
+        _profilePic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Clicked on profile tab item");
+                try {
+                    fragmentTransaction.add(R.id.fragContainer, privateProfileFragment);
+//                    fragmentTransaction.addToBackStack(null);
+                    fragmentTransaction.commit();
+                } catch (Exception e) {
+                    System.out.println("Profile tab item exception");
+                }
+//                fragmentTransaction.replace(R.id.fragContainer, privateProfileFragment);
+//                fragmentTransaction.commit();
             }
         });
 

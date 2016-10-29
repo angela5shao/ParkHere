f96d431fa9c1300863f8c3089df677968f51c12e
Yanbin Lyu <yanbinlu@usc.edu>
2016-10-29 14:28:00 -0700

    added client

--- app/src/main/java/csci310/parkhere/controller/ClientCommunicator.java
+++ app/src/main/java/csci310/parkhere/controller/ClientCommunicator.java
@@ -4,5 +4,22 @@ package csci310.parkhere.controller;
  * Created by angela02pd2014 on 10/16/16.
  */
 
-public class ClientCommunicator {
+public class ClientCommunicator extends Thread{
+    private Socket socket;
+    private ObjectInputStream ois;
+    private ObjectOutputStream oos;
+    public ClientCommunicator(){
+        try {
+            socket = new Socket(hostname, 61129);
+//            System.out.println("Connected!");
+            ois = new ObjectInputStream(socket.getInputStream());
+            oos = new ObjectOutputStream(socket.getOutputStream());
+            start();
+        } catch (IOException ioe) {
+            System.out.println("IOE in Client constructor: " + ioe.getMessage());
+        }
+    }
+    public void send(String command, HashMap<String, Object> entry){
+        oos.writeObject();
+    }
 }

509cce90f6de505c5eab78dd2d2eaadb56d8dc9d
Yanbin Lyu <yanbinlu@usc.edu>
2016-10-29 13:14:46 -0700

    added NetworkPackage

---
+++ app/src/main/java/csci310/parkhere/resource/NetworkPackage.java
@@ -0,0 +1,44 @@
+package resource;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class NetworkPackage implements Serializable
+{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7386596359060429547L;
+	private MyEntry<String, Object> entry;
+	private User user;
+	
+	
+	public NetworkPackage() {
+		user = null;
+		entry = null;
+	}
+	
+	public NetworkPackage(User u) {
+		user = u;
+		entry = null;
+	}
+	
+	public void addEntry(String command, Object obj) {
+		entry = new MyEntry<String, Object>(command, obj);
+	}
+	
+	public MyEntry<String, Object> getCommand(){
+		return entry;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+}

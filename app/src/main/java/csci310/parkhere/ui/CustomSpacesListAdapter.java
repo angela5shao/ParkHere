1989f003e41075bad5b4b325d103c921b896789a
Angela Shao <annchies@usc.edu>
2016-10-31 22:20:08 -0700

    merge with reservation-detail

---
+++ app/src/main/java/csci310/parkhere/ui/CustomSpacesListAdapter.java
@@ -0,0 +1,10 @@
+package csci310.parkhere.ui;
+
+import android.widget.ArrayAdapter;
+
+/**
+ * Created by angela02pd2014 on 10/31/16.
+ */
+
+public class CustomSpacesListAdapter extends ArrayAdapter {
+}

ac56e22131bdb1b5bc03c6bccd15df381c421112
ivylinlaw <ivylin@usc.edu>
2016-11-07 15:19:15 -0800

    pull from angela

--- app/src/androidTest/java/csci310/parkhere/ui/LoginTest.java
+++ app/src/androidTest/java/csci310/parkhere/ui/LoginTest.java
@@ -2,53 +2,28 @@ package csci310.parkhere.ui;
 
 import android.app.Activity;
 import android.app.Instrumentation;
-import android.content.ComponentName;
 import android.content.Intent;
-import android.support.test.espresso.assertion.ViewAssertions;
 import android.support.test.espresso.intent.rule.IntentsTestRule;
-import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.filters.LargeTest;
-import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import csci310.parkhere.R;
-import csci310.parkhere.ui.HomeActivity;
-import csci310.parkhere.ui.LoginActivity;
-import csci310.parkhere.ui.ProviderActivity;
-import csci310.parkhere.ui.RegisterMainActivity;
-import csci310.parkhere.ui.RegisterRenterActivity;
-import csci310.parkhere.ui.RenterActivity;
 
 import static android.support.test.InstrumentationRegistry.getInstrumentation;
-import static android.support.test.InstrumentationRegistry.getTargetContext;
-import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.intent.Intents.intended;
 import static android.support.test.espresso.intent.Intents.intending;
-import static android.support.test.espresso.intent.Intents.times;
-import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.toPackage;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withSpinnerText;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.anyOf;
-import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.instanceOf;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
--- app/src/androidTest/java/csci310/parkhere/ui/SearchTest.java
+++ app/src/androidTest/java/csci310/parkhere/ui/SearchTest.java
@@ -1,12 +1,16 @@
 package csci310.parkhere.ui;
 
+import android.support.test.espresso.UiController;
+import android.support.test.espresso.ViewAction;
+import android.support.test.espresso.contrib.PickerActions;
+import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.filters.LargeTest;
-//import android.support.test.espresso.contrib.PickerActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
 import android.widget.DatePicker;
-import android.widget.TimePicker;
 
+import org.hamcrest.Matcher;
 import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Rule;
@@ -19,10 +23,12 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
-//import static android.support.test.espresso.contrib.PickerActions;
-import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+//import android.support.test.espresso.contrib.PickerActions;
+//import static android.support.test.espresso.contrib.PickerActions;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
@@ -36,6 +42,25 @@ public class SearchTest {
     public ActivityTestRule<RenterActivity> mActivityRule = new ActivityTestRule(RenterActivity.class);
 
     @Before
+    public static ViewAction setDate(final int day, final int month, final int year) {
+        return new ViewAction() {
+            @Override
+            public void perform(UiController uiController, View view) {
+                ((DatePicker) view).updateDate(year, month, day);
+            }
+
+            @Override
+            public String getDescription() {
+                return "Set the date into the datepicker(day, month, year)";
+            }
+
+            @Override
+            public Matcher<View> getConstraints() {
+                return ViewMatchers.isAssignableFrom(DatePicker.class);
+            }
+        };
+    }
+
     public void init() {
         // From HomeActivity, click on Login
         onView(withId(R.id.loginButton)).perform(click());
@@ -46,9 +71,32 @@ public class SearchTest {
         onView(withId(R.id.loginButton)).perform(click());
 
         // TODO: If in ProviderActivity, switch to RenterActivity
+        intended(hasComponent(RenterActivity.class.getName()));
+
+        // TODO: If RenterActivity check if SearchFragment
+
+        // TODO: Click on Search on ActionBar and check if SearchFragment
 
         // Search: type address
         onView(withId(R.id.emailText)).perform(typeText(EMAIL_TO_BE_TYPED));
+//        AutoCompleteTextView autoComplete = (AutoCompleteTextView) findViewById(
+//                R.id.auto_complete_text_view);
+//        String [] completions = new String[] {
+//                "Pacific Ocean", "Atlantic Ocean", "Indian Ocean", "Southern Ocean", "Artic Ocean",
+//                "Mediterranean Sea", "Caribbean Sea", "South China Sea", "Bering Sea",
+//                "Gulf of Mexico", "Okhotsk Sea", "East China Sea", "Hudson Bay", "Japan Sea",
+//                "Andaman Sea", "North Sea", "Red Sea", "Baltic Sea" };
+//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+//                android.R.layout.simple_dropdown_item_1line,
+//                completions);
+//        autoComplete.setAdapter(adapter);
+
+        // Change the date of the DatePicker. Don't use "withId" as at runtime Android shares the DatePicker id between several sub-elements
+
+        //btn_start_date
+        onView(withClassName(Matchers.equalTo(DatePicker.class.getName()))).perform(PickerActions.setDate(2016, 8, 25));
+        // Click on the "OK" button to confirm and close the dialog
+        onView(withText("OK")).perform(click());
 
 //        // Select start date & time
 //        onView(withId(R.id.btn_start_date)).perform(click());

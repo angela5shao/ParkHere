0ff4de5e26e16c002624b1bf3a31898c30125f3f
Yanbin Lyu <yanbinlu@usc.edu>
2016-11-07 21:20:19 -0800

    the peace before storm

--- app/src/androidTest/java/csci310/parkhere/ui/UserProfileTest.java
+++ app/src/androidTest/java/csci310/parkhere/ui/UserProfileTest.java
@@ -14,6 +14,7 @@ import csci310.parkhere.R;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
@@ -24,9 +25,10 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
 public class UserProfileTest {
     public static final String EMAIL_TO_BE_TYPED = "yy@yy.com"; //"espressoeast@usc.edu";
     public static final String PASSWORD_CORRECT_TO_BE_TYPED = "1234567890"; //"12345";
-    public static final String LAST_USER_ROLE = "Provider";
-    public static final String LICENSE_TO_BE_TYPED = "0909090";
-    public static final String LICENSE_PLATE_TO_BE_TYPED = "089089089";
+//    public static final String LAST_USER_ROLE = "Provider";
+    public static final String LICENSE_TO_BE_TYPED = "747234";
+    public static final String LICENSE_PLATE_TO_BE_TYPED = "7MOP210";
+    public static final String PHONE_TO_BE_TYPED = "7472346798";
 
     @Rule
     public ActivityTestRule<HomeActivity> mActivityRule = new ActivityTestRule(HomeActivity.class);
@@ -48,33 +50,44 @@ public class UserProfileTest {
         onView(withId(R.id.profilePic)).perform(click());
         onView(withId(R.id.usernameText)).check(matches(withText(EMAIL_TO_BE_TYPED)));
         // TODO: check licence ID & licence plate
-//        onView(withId(R.id.licenseIDText)).check(matches(withText(LICENSE_TO_BE_TYPED)));
-//        onView(withId(R.id.licenseplateText)).check(matches(withText(LICENSE_PLATE_TO_BE_TYPED)));
+        onView(withId(R.id.licenseIDText)).check(matches(withText(LICENSE_TO_BE_TYPED)));
+        onView(withId(R.id.licenseplateText)).check(matches(withText(LICENSE_PLATE_TO_BE_TYPED)));
+        onView(withId(R.id.phoneText)).check(matches(withText(PHONE_TO_BE_TYPED)));
     }
 
     @Test
     public void editPasswordLongPhoneNumberLong() {
-
+        // Click on Profile and validate email
+        onView(withId(R.id.profilePic)).perform(click());
+        onView(withId(R.id.editLogo)).perform(click());
+        // Type name, email, password, phone.
+        onView(withId(R.id.pwText)).perform(typeText("1234567890"), closeSoftKeyboard());
+        onView(withId(R.id.licenseIDText)).perform(typeText(LICENSE_TO_BE_TYPED), closeSoftKeyboard());
+        onView(withId(R.id.licenseplateText)).perform(typeText("089089098"), closeSoftKeyboard());
+        onView(withId(R.id.phoneText)).perform(typeText(PHONE_TO_BE_TYPED), closeSoftKeyboard());
+        onView(withId(R.id.btn_save)).perform(scrollTo()).perform(click());
+        //click on the private profile button again and check whether the data is correct
+        onView(withId(R.id.profilePic)).perform(click());
+        onView(withId(R.id.usernameText)).check(matches(withText(EMAIL_TO_BE_TYPED)));
+        onView(withId(R.id.licenseIDText)).check(matches(withText(LICENSE_TO_BE_TYPED)));
+        onView(withId(R.id.licenseplateText)).check(matches(withText("089089098")));
+        onView(withId(R.id.phoneText)).check(matches(withText(PHONE_TO_BE_TYPED)));
     }
 
     @Test
     public void editPasswordShort() {
-
+        // Click on Profile and validate email
+        onView(withId(R.id.profilePic)).perform(click());
+        onView(withId(R.id.editLogo)).perform(click());
+        // Type name, email, password, phone.
+        onView(withId(R.id.passwordText)).perform(typeText("123456789"), closeSoftKeyboard());
+        onView(withId(R.id.licenseIDText)).perform(typeText(LICENSE_TO_BE_TYPED), closeSoftKeyboard());
+        onView(withId(R.id.licenseplateText)).perform(typeText("089089098"), closeSoftKeyboard());
+        onView(withId(R.id.phoneText)).perform(typeText(PHONE_TO_BE_TYPED), closeSoftKeyboard());
     }
 
     @Test
     public void editPhoneNumberShort() {
 
-    }
-
-    @Test
-    public void checkPublicProfileInfo() {
-        // Assuming loggedin as Renter
-        // TODO:
-        // Search for my own parking spot
-
-        // Click to get search detail
-
-        // Click to get public profile & validate information (first name)
     }
 }

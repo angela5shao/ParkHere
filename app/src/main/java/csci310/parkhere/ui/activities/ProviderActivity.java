4b88be906b23ec58b30f68ab25a51ac91beddb5e
ivylinlaw <ivylin@usc.edu>
2016-11-29 21:42:02 -0800

    merge

--- app/src/main/java/csci310/parkhere/ui/activities/ProviderActivity.java
+++ app/src/main/java/csci310/parkhere/ui/activities/ProviderActivity.java
@@ -271,9 +271,6 @@ public class ProviderActivity extends AppCompatActivity implements SpacesFragmen
         return true;
     }
 
-
-
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
